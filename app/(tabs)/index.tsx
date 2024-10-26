7189511af3f0113f4fbbcfd74d8f308d3ffd6818
Mohammad Faizan <mofaizan.xo@gmail.com>
2024-10-26 18:27:49 +0500

    Initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,40 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { StyleSheet, Image, Text, View } from 'react-native';
+import Food from  '@/assets/Data/Food';//This line imports Food array data from  the Food.tsx file
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <Image
+        source={{ uri: Food[0].image }}
+        style={styles.image}
+      />
+      <Text style={styles.titleText}>{Food[0].title}</Text>
+      <Text style={styles.titleText}>{Food[0].price}</Text>
+      <Image
+        source={{ uri: Food[1].image }}
+        style={styles.image}
+      />
+      <Text style={styles.titleText}>{Food[1].title}</Text>
+      <Text style={styles.titleText}>{Food[1].price}</Text>
+    </View>
   );
 }
 
+//Styling like in html css
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
+    backgroundColor: '#F5FCFF',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  titleText: {
+    fontSize: 30,
+    color: 'black',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  image: {
+    width: '100%',
+    aspectRatio: 2/1,
+    marginBottom: 5,
   },
 });
---
+++ assets/Data/Food.tsx
@@ -0,0 +1,17 @@
+//Food ArrayDeclaration
+const Food = [
+    {//array element 1
+      id: 0,
+      title: 'Pizza',
+      price: '14$',
+      image: 'https://media.istockphoto.com/id/187248625/photo/pepperoni-pizza.jpg?s=612x612&w=is&k=20&c=Mx1YTfJ1W3SZDpJxYbPeFp9rWVl1QlM1_krBgHQpNA0='
+    },
+    {//array element 2
+      id: 1,
+      title: 'Burger',
+      price: '10$',
+      image: 'https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_1280.jpg'
+    }
+  ];
+
+  export default Food;
